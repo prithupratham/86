954fd6d3f5b3246386a4ee111e8340dd4e8134b9
prithupratham <84316410+prithupratham@users.noreply.github.com>
2021-07-16 14:41:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+block_width = 30;
+block_height = 30;
+
+player_x = 10;
+player_y = 10;
+
+var block_imgobject = "";
+var player_object = "";
+
+function player_update(){
+fabric.Image.fromURL("player.png",function (Img){
+player_object = Img;
+
+player_object.scaleToWidth(150);
+player_object.scaleToHeight(140);
+player_object.set({
+top:player_y,
+left:player_x
+});
+canvas.add(player_object);
+});
+}
+function new_image(get_image){
+    fabric.Image.fromURL(get_image,function (Img){
+    block_imgobject = Img;
+    
+    block_imgobject.scaleToWidth(block_width);
+    block_imgobject.scaleToHeight(block_height);
+    block_imgobject.set({
+    top:player_y,
+    left:player_x
+    });
+    canvas.add(block_imgobject);
+    });
+    }
